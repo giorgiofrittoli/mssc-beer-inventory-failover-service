4558c388190a26fcad9fb459a40cd3f72d833c42
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-14 10:10:36 +0200

    init commit

---
+++ src/main/java/it/frigir/msscbeerinventoryfailoverservice/MsscBeerInventoryFailoverServiceApplication.java
@@ -0,0 +1,13 @@
+package it.frigir.msscbeerinventoryfailoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerInventoryFailoverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerInventoryFailoverServiceApplication.class, args);
+	}
+
+}
