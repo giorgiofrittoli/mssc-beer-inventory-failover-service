d1043b2b94a491b731ae6eeb5f73379f05b7ff49
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-07-14 11:09:09 +0200

    fix failover service

--- src/main/java/it/frigir/msscbeerinventoryfailoverservice/web/InventoryHandler.java
+++ src/main/java/it/frigir/msscbeerinventoryfailoverservice/web/InventoryHandler.java
@@ -10,6 +10,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("api/v1/inventory-failover")
@@ -20,13 +22,13 @@ public class InventoryHandler {
     public Mono<ServerResponse> listInventory(ServerRequest serverRequest) {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_STREAM_JSON)
-                .body(Mono.just(BeerInventoryDto.builder()
+                .body(Mono.just(Arrays.asList(BeerInventoryDto.builder()
                                 .id(UUID.randomUUID())
                                 .beerId(UUID.fromString("00000000-00000000-00000000-00000000"))
                                 .quantityOnHand(999)
                                 .createdDate(OffsetDateTime.now())
-                                .lastModifiedDate(OffsetDateTime.now()).build()),
-                        BeerInventoryDto.class);
+                                .lastModifiedDate(OffsetDateTime.now()).build())),
+                        List.class);
     }
 
 }
